2de9894ecb1bf432c6f88569ab4aeefe8b548acd
Robbe Sneyders <robbe.sneyders@ml6.eu>
2022-03-18 18:44:35 +0100

    Remove example change

--- examples/openapi3/helloworld/hello.py
+++ examples/openapi3/helloworld/hello.py
@@ -8,6 +8,5 @@ def post_greeting(name: str) -> str:
 
 if __name__ == '__main__':
     app = connexion.FlaskApp(__name__, port=9090, specification_dir='openapi/')
-    app.add_api('helloworld-api.yaml', arguments={'title': 'Hello World Example'},
-                options={"serve_spec": False})
+    app.add_api('helloworld-api.yaml', arguments={'title': 'Hello World Example'})
     app.run()
